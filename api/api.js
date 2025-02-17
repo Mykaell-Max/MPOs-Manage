0182b2fe3f6963f8fb31d67fe927ebcfc0481b7a
Mykaell Max <max000.256.000@gmail.com>
2025-02-17 15:24:31 -0300

    Basic user CRUD logic

--- api/api.js
+++ api/api.js
@@ -2,12 +2,12 @@ const express = require("express");
 
 const api = express();
 
-const operatorRoutes = require('../api/routes/operatorRoutes');
+const userRoutes = require('../api/routes/userRoutes');
 const osRoutes = require('../api/routes/osRoutes');
 
 api.use(express.json());
 
-api.use('/api/operator', operatorRoutes);
+api.use('/api/user', userRoutes);
 api.use('/api/os', osRoutes);
 
 api.listen(8888, () => {console.log('Service running...')});
--- api/routes/userRoutes.js
+++ api/routes/userRoutes.js
@@ -3,13 +3,19 @@ const router = express.Router();
 
 const userController = require('../controller/userController');
 
+const {verifyJWT} = require('../../middlewares/jwtAuth');
+
 router
     .route('/registerUser')
     .post(userController.createUser);
 
+router
+    .route('/login')
+    .post(userController.loginUser);
 
-
-
-// login, update, delete | too
+router
+    .route('/:userId')
+    .patch(userAuth, userController.updateUser)
+    .delete(userAuth, userController.deleteUser);
 
 module.exports = router;
